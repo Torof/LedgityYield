bebbb708e440bcc6ba9b2100fe51b5a7ddaf3bb2
torof <scottpanam@protonmail.com>
2024-07-19 11:59:09 +0900

    style(wider widget): made the widget component wider using tailwind instead of inline

--- src/components/app/swap/AppSwap.tsx
+++ src/components/app/swap/AppSwap.tsx
@@ -1,9 +1,8 @@
 import '@swing.xyz/ui/theme.css';
 import { Swap } from '@swing.xyz/ui';
-import "./AppSwap.css";
 
 export function AppSwap() {
-  return <div id="swing">
+  return <div className="w-[800px]">
       <Swap projectId="swing-bridge" environment='production' />
     </div>
 }
